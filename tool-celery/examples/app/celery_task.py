12695c1cad3254b60ac011b1ab8f253dd0210192
席刚 <hustcsxg@163.com>
2019-10-08 15:31:30 +0800

    celery使用帮助

--- tool-celery/examples/app/celery_task.py
+++ tool-celery/examples/app/celery_task.py
@@ -1,4 +1,15 @@
 from . import celery
-@celery.task()
-def add_together(a, b):
+
+# i f bind=Ture first param must be self
+@celery.task(bind=True)
+def add_together(self, a, b):
+    # 获取celery task id
+    task_exec_id = self.request.id
+    print(task_exec_id)
     return a + b
+
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    add_together.delay(a, b)
